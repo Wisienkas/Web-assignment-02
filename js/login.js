e810e0be98ee623eab4d9204d6e2cc448c8c2828
Nikolaj Schaldemose Reibke <nikolajsr12@gmail.com>
2015-04-11 19:33:54 +0200

    Login system works

--- js/login.js
+++ js/login.js
@@ -4,29 +4,45 @@
 Parse.initialize("qjnTttFV6ZzF8BXJ7qoluCOHG2yUGg0tKtRC6RdK", "z3txBOG5ywRRIMUhqIlu9f5eCXSzEyTNqo57ycRI");
 
 function login(usr, pass) {
+    if(Parse.User.current()) {
+        alert("You are already Logged In, Loggin user current user out!");
+        Parse.User.logOut();
+    }
     Parse.User.logIn(usr, pass, {
-
+        success: function(user) {
+            // Do stuff after successful login.
+            alert("Login success!");
+            window.location.href = "index.html";
+            self.undelegateEvents();
+            delete self;
+        },
+        error: function(user, error) {
+            // The login failed. Check error to see why.
+            alert("Username or Password is Wrong");
+        }
     });
 }
 
+function showUser() {
+    var user = Parse.User.current();
+    if(user) {
+        console.log("User logged in as: " + user.getUsername());
+    } else {
+        console.log("No user is logged")
+    }
+}
+
 $("document").ready(function() {
-   $("#login-btn").click(function() {
-       if(Parse.User.current()) {
-           alert("You are already Logged In, Please log out first");
-           Parse.User.logOut();
-       }
-       alert("alert 2");
-       Parse.User.logIn($("#inputUsername").val(), $("#inputPassword").val(), {
-           success: function(user) {
-               // Do stuff after successful login.
-               alert("Login success!");
-           },
-           error: function(user, error) {
-               // The login failed. Check error to see why.
-               alert("Login Failed with Error: " + error + " User: " + user);
-           }
-       });
-   })
+
+    $("#login-form").submit(function(event) {
+        event.preventDefault();
+    });
+
+    $("#login-btn").click(function() {
+        login($("#inputUsername").val(), $("#inputPassword").val());
+    })
+
+    showUser();
 });
 
 
