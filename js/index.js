3c9e8b9a0b3833e7275b3385e296bf8c7f756950
Nikolaj Reibke <nikolajsr12@gmail.com>
2015-04-13 00:49:37 +0200

    Added highlight for relations of country

--- js/index.js
+++ js/index.js
@@ -6,7 +6,6 @@ Parse.initialize("qjnTttFV6ZzF8BXJ7qoluCOHG2yUGg0tKtRC6RdK", "z3txBOG5ywRRIMUhqI
 //$(document).ready(function() {
 //    $("p").appendTo("\nUser Status: " + Parse.User.getUsername());
 //});
-Parse.User.current().get("username");
 
 // Getting object factories
 var Country = Parse.Object.extend("Country");
@@ -16,10 +15,20 @@ function fetchCountries() {
     var query = new Parse.Query(Country);
     query.find({
         success: function(results) {
-
+            for(i = 0; i < results.length; i++) {
+                var id = results[i].id;
+                var country = '<li id="' + id + '"class="list-group-item country"><p>' + results[i].get("name") + '</p></li>';
+                $("#countries").append(country);
+                $("#" + id).click(function() {
+                    $(".country").removeClass("highlight");
+                    $(".food").removeClass("highlight");
+                    $("#" + this.id).addClass("highlight");
+                    $('.' + this.id).addClass("highlight");
+                });
+            }
         },
         error: function(error) {
-
+            Alert("Failed to retrieve Countries");
         }
     })
 }
@@ -28,13 +37,18 @@ function fetchFoods() {
     var query = new Parse.Query(Food);
     query.find({
         success: function(results) {
-
+            for(i = 0; i < results.length; i++) {
+                $("#foods").append('<li class="list-group-item food ' + results[i].get("parent").id + '"><p>' + results[i].get("name") + '</p></li>')
+            }
         },
         error: function(error) {
-
+            Alert("Failed to retrieve Foods");
         }
     })
 }
+
+fetchCountries();
+fetchFoods();
 
 // Both have "Name" to be shown
 function addItemToList(listId, item) {
